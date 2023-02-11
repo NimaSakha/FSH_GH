349e107eda6e1f41c6a334b7e0dc56e0efce0342
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2023-02-11 15:01:46 +1100

    Update App.jsx
    App.jsx Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
 function App() {
-  return null
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
 }
 
 export default App
