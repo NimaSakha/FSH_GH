5a1180a0e2315e64ff8d88e998c6d6b44e33e970
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2023-02-11 15:02:27 +1100

    Update tailwind.config.js
    custom params update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "500px",
+      lsm: "650px",
+      llsm: "720px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#005CEE",
+      purple: "#6200FF",
+      white: "#fff",
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
 }
