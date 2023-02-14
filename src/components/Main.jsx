5c8cfc77dbf784250b8ed99d03e733f6facd5970
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2023-02-14 17:58:34 +1100

    Update Main.jsx
    Content added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,33 @@
+import Scroll from "./Scroll"
+import element from "../resources/element.svg"
+import Info from "./Info"
 function Main() {
-  return null
+  return (
+    <>
+      <div className=' h-screen w-screen lsm:w-4/5 gradients'>
+        <div className='mb-32'>
+          <div className='pt-32 pl-20 '>
+            <h1 className='  font-poppins font-semibold text-5xl text-white'>
+              Farshad
+              <br />
+              Ghoratolhamid
+            </h1>
+            <p className=' mt-3 text-white font-poppins'>Scroll 👇</p>
+            <Scroll style='mt-4' />
+          </div>
+          <img
+            src={element}
+            className='hidden lsm:flex w-80 m-auto -mr-40 -mt-52'
+            alt=''
+          />
+        </div>
+
+        <Info />
+        <div className=' hidden lsm:flex bg-white h-20 wdth absolute right-0 m-auto btm rounded-bl-3xl'></div>
+        <div className='h-screen w-screen  gradients2'></div>
+      </div>
+    </>
+  )
 }
 
 export default Main
