60933bd3fded9cc559928101c6fa6c1327de8f63
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2023-02-14 17:58:23 +1100

    Create Scroll.jsx

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,11 @@
+function Scroll({ style }) {
+  return (
+    <div className={style}>
+      <div className='field'>
+        <div className='mouse'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Scroll
