5ee8cb6a8acd4326706ecd4d183bd218f642e154
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2023-02-14 17:58:43 +1100

    Update Header.jsx
    Header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import instagram from "../resources/Instagram.svg"
 function Header() {
   return (
     <>
-      <div className='flex justify-items-stretch'>
-        <div className=' lsm:w-4/5 bg-purple'>
-          <div className='items-center lsm:w-auto rounded-none w-screen just flex-col lsm:flex-row  p-2  bg-blue lsm:rounded-br-3xl  flex justify-between justify-items-center'>
+      <div className='flex justify-items-stretch h-fit '>
+        <div className=' lsm:w-4/5 bg-back '>
+          <div className=' h-full items-center lsm:w-auto rounded-none w-screen just flex-col lsm:flex-row  p-3  bg-orange lsm:rounded-br-3xl  flex justify-between justify-items-center shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]'>
             <h1 className=' text-center font-poppins font-bold text-white '>
               Farshad Ghoratolhamid
             </h1>
